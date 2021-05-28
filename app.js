2f3819fc098d8d58bcb631d7bee593bfa161b5ff
joe sellers <josephdavidsellers@gmail.com>
2021-05-28 23:26:37 +0100

    commit

--- app.js
+++ app.js
@@ -9,5 +9,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
+app.get('/', function(req, res){
+    res.render('client\index.js');
+  });
+  
 
-
+app.listen(process.env.PORT, console.log('App is running on ' + process.env.PORT));
