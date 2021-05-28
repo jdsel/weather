d8ca6c407b887838f92fdf56dac3ab42c5382525
joe sellers <josephdavidsellers@gmail.com>
2021-05-28 23:12:09 +0100

    commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+
+
